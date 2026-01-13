6331ec755b36c45253375fb9a75079687f8a40b7
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-01-13 20:48:58 +0100

    practice

--- 1- Getting Started/course-starting-project/app.ts
+++ 1- Getting Started/course-starting-project/app.ts
@@ -5,6 +5,15 @@
 const person = {
   name: "M",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.age);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLocaleUpperCase());
+  // console.log(hobby.map());
+}
