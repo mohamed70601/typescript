7f6a6c95af997805ab74aa9524dce5bdc783062c
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-01-16 23:42:10 +0100

    enum practice

--- 1- Getting Started/course-starting-project/app.ts
+++ 1- Getting Started/course-starting-project/app.ts
@@ -2,20 +2,33 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "M",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "M",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
-person.role[0] = 10;
+// person.role[0] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
