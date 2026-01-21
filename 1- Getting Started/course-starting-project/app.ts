b232768981888efab55be78091914f2264269f64
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-01-21 19:18:37 +0100

    unknown type

--- 1- Getting Started/course-starting-project/app.ts
+++ 1- Getting Started/course-starting-project/app.ts
@@ -1,24 +1,9 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Moh";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-printResult(add(5, 12));
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
