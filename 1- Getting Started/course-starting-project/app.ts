f45bd66758aa05087c530508b0ec13d88fcd8dca
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-01-21 19:14:26 +0100

    function types callbacks

--- 1- Getting Started/course-starting-project/app.ts
+++ 1- Getting Started/course-starting-project/app.ts
@@ -7,3 +7,18 @@ function printResult(num: number): void {
 }
 
 printResult(add(5, 12));
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
