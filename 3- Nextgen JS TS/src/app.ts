3fd044492d49ec69ccc6bdacf6071e99e3c4859b
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-01-27 22:18:55 +0100

    arrow func

--- 3- Nextgen JS TS/src/app.ts
+++ 3- Nextgen JS TS/src/app.ts
@@ -4,13 +4,26 @@ const userName = "Moh";
 let age = 30;
 age = 29;
 
-function add(a: number, b: number) {
-  var result;
-  result = a + b;
-  return result;
+// function add(a: number, b: number) {
+//   var result;
+//   result = a + b;
+//   return result;
+// }
+
+// if (age > 20) {
+//   let isOld = true;
+//   console.log(isOld);
+// }
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
 }
 
-if (age > 20) {
-  let isOld = true;
-  console.log(isOld);
-}
+printOutput(add(5, 2));
