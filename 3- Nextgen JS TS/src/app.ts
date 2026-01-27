a9f354d547d1ed081cc44498c6b187eb339f34d6
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-01-27 20:59:18 +0100

    let & const

--- 3- Nextgen JS TS/src/app.ts
+++ 3- Nextgen JS TS/src/app.ts
@@ -1,1 +1,16 @@
-// Code goes here!
+const userName = "Moh";
+// userName = "Mohamed"
+
+let age = 30;
+age = 29;
+
+function add(a: number, b: number) {
+  var result;
+  result = a + b;
+  return result;
+}
+
+if (age > 20) {
+  let isOld = true;
+  console.log(isOld);
+}
