083a861f4113afabea166a558fa88281e9b515ac
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-01-31 23:14:24 +0100

    private & public access

--- 4- Classes interfaces/src/app.ts
+++ 4- Classes interfaces/src/app.ts
@@ -1,11 +1,31 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accouting = new Department("Accounting");
 
-console.log(accouting);
+accouting.addEmployee("Moh");
+accouting.addEmployee("Manu");
+
+// accouting.employees[2] = "Anna";
+
+accouting.describe();
+accouting.printEmployeeInformation();
