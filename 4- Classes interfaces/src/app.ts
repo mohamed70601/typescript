4b49d28d4bfce2c5426f0f82063aa83baf934a1d
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-02-07 22:45:04 +0100

    interfaces with classes

--- 4- Classes interfaces/src/app.ts
+++ 4- Classes interfaces/src/app.ts
@@ -1,18 +1,25 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 29;
 
-user1 = {
-  name: "Moh",
-  age: 29,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Moh");
 
 user1.greet("Hi there - I am");
+console.log(user1);
