54fad482c24cfc1f549f3e5ffbae7c3f580951d1
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-02-13 19:28:00 +0100

    type casting

--- 5- Advanced types/src/app.ts
+++ 5- Advanced types/src/app.ts
@@ -99,3 +99,15 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+const userInputElement = document.getElementById(
+  "user-input",
+)! as HTMLInputElement;
+
+// userInputElement.value = "Hi there!";
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
+}
