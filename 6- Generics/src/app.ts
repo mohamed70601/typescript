3e47c706b6e2825978786b67f99317150d059d84
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2026-02-21 21:41:26 +0100

    generic utility types

--- 6- Generics/src/app.ts
+++ 6- Generics/src/app.ts
@@ -77,3 +77,25 @@ const numberStorage = new DataStorage<number>();
 
 // objStorage.removeItem(mohObj);
 // console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date,
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Moh", "Anna"];
+// names.push("Manu");
+// names.pop();
